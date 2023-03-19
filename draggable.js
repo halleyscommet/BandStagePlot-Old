6d91e7e7b36c866469ab8a0516da1d8f972866ca
Noah Harper <114954963+noahlikesvr@users.noreply.github.com>
2023-03-18 23:45:23 -0400

    Fix duplication glitch

--- draggable.js
+++ draggable.js
@@ -1,25 +1,3 @@
-$(function() {
-    // Make the elements draggable
-    $("#drum-draggable, #e-guitar-draggable, #b-guitar-draggable, #microphone-stand-draggable, #singer-draggable, #g-amp-draggable, #b-amp-draggable, #p-board-draggable")
-        .draggable()
-        .dblclick(function(event) {
-            // Clone the element on double click
-            var $clone = $(this).clone();
-            
-            // Set the clone's position to be offset from the original element
-            $clone.offset({
-                top: event.pageY - 20,
-                left: event.pageX - 20
-            });
-            
-            // Make the clone draggable
-            $clone.draggable();
-            
-            // Add the clone to the page
-            $(this).after($clone);
-        });
-});
-
 function resetPosition() {
     // Reset the position of the elements
     $("#drum-draggable, #e-guitar-draggable, #b-guitar-draggable, #microphone-stand-draggable, #singer-draggable, #g-amp-draggable, #b-amp-draggable, #p-board-draggable")
@@ -33,12 +11,6 @@ $(function() {
         .dblclick(function(event) {
             // Clone the element on double click
             var $clone = $(this).clone();
-            
-            // Set the clone's position to be offset from the original element
-            $clone.offset({
-                top: event.pageY - 20,
-                left: event.pageX - 20
-            });
             
             // Make the clone draggable
             $clone.draggable();
