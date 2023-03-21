040bfc04a788c0d27cda1346a503a6447a0d18eb
Noah Harper <114954963+noahlikesvr@users.noreply.github.com>
2023-03-21 12:20:06 -0400

    Stable

---
+++ JS/import.js
@@ -0,0 +1,16 @@
+function addImage(){
+    let image = document.querySelector('input[type="file"]').files[0];
+    let imageName = image.name;
+    let allowedTypes = ["png"];
+    let imageType = imageName.substring(imageName.lastIndexOf('.') + 1).toLowerCase();
+    if(allowedTypes.indexOf(imageType) != -1){
+        let reader = new FileReader();
+        reader.onload = function(){
+            let imageContainer = document.getElementById("import-draggable");
+            let newImage = document.createElement("img");
+            newImage.setAttribute("src",reader.result);
+            imageContainer.appendChild(newImage);
+        }
+        reader.readAsDataURL(image);
+    }
+}
